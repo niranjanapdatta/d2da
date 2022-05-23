06999f6defa001baee3be53e17e8be91d3364767
Datta <ndatta@netapp.com>
2022-05-24 00:25:03 +0530

    picks_bans data cleaning

--- app/main.py
+++ app/main.py
@@ -12,12 +12,17 @@ def addToRow(fields, data, row=[]):
             row.append(None)
     return row
 
-def getMatchesDataCsv(account_id, query_params={}, api_calls_limit=60, api_limit_duration_seconds=60):
+def dictKeyToSortBy(item):
+  return item['order']
+
+def getMatchesDataCsv(account_id, query_params={}, api_calls_limit=50, api_limit_duration_seconds=60):
+    count = 0
     api_calls_made = 0
     rows = []
-    match_fields = ['match_id', 'duration', 'first_blood_time', 'dire_score', 'radiant_score', 'radiant_win', 'start_time', 'region', 'replay_url', 'picks_bans']
+    match_fields = ['match_id', 'duration', 'first_blood_time', 'game_mode', 'dire_score', 'radiant_score', 'radiant_win', 'start_time', 'region', 'replay_url']
     player_fields = ['player_slot', 'ability_upgrades_arr', 'assists', 'backpack_0', 'backpack_1', 'backpack_2', 'deaths', 'denies', 'gold', 'gold_spent', 'hero_damage', 'hero_healing', 'hero_id', 'item_0', 'item_1', 'item_2', 'item_3', 'item_4', 'item_5', 'item_neutral', 'kills', 'last_hits', 'level', 'net_worth', 'permanent_buffs', 'tower_damage', 'isRadiant', 'total_gold', 'total_xp', 'kda', 'rank_tier']
     benchmark_fields = ['gold_per_min', 'xp_per_min', 'kills_per_min', 'last_hits_per_min', 'hero_damage_per_min', 'hero_healing_per_min', 'tower_damage']
+    other_fields = ['list_of_picks']
     filename = f"player-{account_id}-matches.csv"
     matches_data = requests.get(f'https://api.opendota.com/api/players/{account_id}/matches', params=query_params)
     api_calls_made += 1
@@ -26,25 +31,57 @@ def getMatchesDataCsv(account_id, query_params={}, api_calls_limit=60, api_limit
     for match in matches_data_json:
         if api_calls_made < api_calls_limit:
             row = []
-            match_id = match['match_id']
+            match_id = None
+            try:
+                match_id = match['match_id']
+            except KeyError:
+                print('Could not get match_id')
+                continue
             match_data_detailed = requests.get(f'https://api.opendota.com/api/matches/{match_id}')
             api_calls_made += 1
             data = json.loads(match_data_detailed.text)
             row = addToRow(match_fields, data, row)
-            for player in data['players']:
-                if player['account_id'] == account_id:
-                    row = addToRow(player_fields, player, row)
-                    row = addToRow(benchmark_fields, player['benchmarks'], row)
+            picks_hero_ids = []
+            picks_hero_map = {}
+            try:
+                for pick_ban in data['picks_bans']:
+                    if pick_ban['is_pick']:
+                        picks_hero_ids.append(pick_ban['hero_id'])
+                        picks_hero_map.update({pick_ban['hero_id']: {'hero_id': pick_ban['hero_id'], 'team': pick_ban['team'], 'order': pick_ban['order']}})
+            except KeyError:
+                print(f'KeyError occured while getting picks_bans data for match with id {match_id}')
+            except TypeError:
+                print(f'TypeError occured while getting picks_bans data for match with id {match_id}')
+            list_of_picks = []
+            try:
+                for player in data['players']:
+                    if player['hero_id'] in picks_hero_ids:
+                        list_of_picks.append(picks_hero_map.get(player['hero_id']))
+                    if player['account_id'] == account_id:
+                        row = addToRow(player_fields, player, row)
+                        row = addToRow(benchmark_fields, player['benchmarks'], row)
+                list_of_picks.sort(key=dictKeyToSortBy)
+            except KeyError:
+                print(f'KeyError occured while getting data of player with account id {account_id} for match with id {match_id}')
+            except TypeError:
+                print(f'TypeError occured while getting picks_bans data for match with id {match_id}')
+            row.append(list_of_picks)
             rows.append(row)
+            print(f'fetched data for match with id: {match_id}')
+            # time.sleep(1) # Wait one second after each api call to avoid traffic
         else:
+            print('**********************')
             print(f'waiting for {api_limit_duration_seconds} seconds before making next set of calls......')
+            print('**********************')
             time.sleep(api_limit_duration_seconds)
             api_calls_made = 0
+        count += 1
+        print(f'{count} matches data have been fetched')
     
     # Write to csv
     with open(filename, 'w', newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(match_fields + player_fields + benchmark_fields)
+        csvwriter.writerow(match_fields + player_fields + benchmark_fields + other_fields)
         csvwriter.writerows(rows)
 
 def main():
