d21dd6f90090f1f05c0aefa58c31e91267223c37
Datta <ndatta@netapp.com>
2022-05-24 22:35:31 +0530

    added new method: getOverallPlayerAnalysis()

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 import logging
 import time
-import requests
 import json
 import time
 import csv
+
+# External Libraries
+import requests
+import pandas as pd
 
 
 def addToRow(fields, data, row=[]):
@@ -14,15 +17,17 @@ def addToRow(fields, data, row=[]):
             row.append(None)
     return row
 
+
 def dictKeyToSortBy(item):
   return item['order']
 
-def getMatchesDataCsv(account_id, query_params={}, api_calls_limit=55, api_limit_duration_seconds=60):
+
+def populatePlayerMatchesDataInCsv(account_id, query_params={}, api_calls_limit=55, api_limit_duration_seconds=60):
     count = 0
     api_calls_made = 0
     rows = []
     match_fields = ['match_id', 'duration', 'first_blood_time', 'game_mode', 'dire_score', 'radiant_score', 'radiant_win', 'start_time', 'region', 'patch', 'throw', 'comeback', 'replay_url']
-    player_fields = ['player_slot', 'ability_upgrades_arr', 'assists', 'backpack_0', 'backpack_1', 'backpack_2', 'deaths', 'denies', 'gold', 'gold_spent', 'hero_damage', 'hero_healing', 'hero_id', 'item_0', 'item_1', 'item_2', 'item_3', 'item_4', 'item_5', 'item_neutral', 'kills', 'last_hits', 'level', 'net_worth', 'permanent_buffs', 'tower_damage', 'isRadiant', 'total_gold', 'total_xp', 'kda', 'rank_tier']
+    player_fields = ['player_slot', 'ability_upgrades_arr', 'assists', 'backpack_0', 'backpack_1', 'backpack_2', 'deaths', 'denies', 'gold', 'gold_spent', 'hero_damage', 'hero_healing', 'hero_id', 'item_0', 'item_1', 'item_2', 'item_3', 'item_4', 'item_5', 'item_neutral', 'kills', 'last_hits', 'level', 'net_worth', 'permanent_buffs', 'isRadiant', 'total_gold', 'total_xp', 'kda', 'rank_tier']
     benchmark_fields = ['gold_per_min', 'xp_per_min', 'kills_per_min', 'last_hits_per_min', 'hero_damage_per_min', 'hero_healing_per_min', 'tower_damage']
     other_fields = ['list_of_picks']
     filename = f"player-{account_id}-matches.csv"
@@ -88,9 +93,59 @@ def getMatchesDataCsv(account_id, query_params={}, api_calls_limit=55, api_limit
         csvwriter.writerows(rows)
         logging.info('data has been written to .csv file')
 
+
+def getOverallPlayerAnalysis(pathToPlayerMatchesDataCsv):
+    print(f'reading data from .csv file. Path: {pathToPlayerMatchesDataCsv}')
+    try:
+        
+        pd.set_option('display.max_rows', None)
+        pd.set_option('display.max_columns', None)
+        df = pd.read_csv(pathToPlayerMatchesDataCsv)
+        print(f'read {len(df.index)} records from .csv file')
+        print('\n')
+        print(f"Player (Account ID: {pathToPlayerMatchesDataCsv.split('-')[1]}) Analysis: ")
+
+        # Total wins and losses
+        match_win_condition_expression = ((df['isRadiant']) & (df['radiant_win'])) | ((df['isRadiant'] == False) & (df['radiant_win'] == False))
+        match_loss_condition_expression = ((df['isRadiant']) & (df['radiant_win'] == False)) | ((df['isRadiant'] == False) & (df['radiant_win']))
+
+        total_wins = df.where(match_win_condition_expression) \
+            .groupby('isRadiant')['radiant_win'] \
+            .count() \
+            .sum()
+        total_losses = df.where(match_loss_condition_expression) \
+            .groupby('isRadiant')['radiant_win'] \
+            .count() \
+            .sum()
+        total_matches = total_wins + total_losses
+        print(f'Total matches played= {total_matches}')
+        print(f'Total wins= {total_wins}')
+        print(f'Total losses= {total_losses}')
+
+        win_percentage = round(((total_wins / total_matches) * 100), 2)
+        loss_percentage = round(((total_losses / total_matches) * 100), 2)
+        print(f'Win percentage= {win_percentage}')
+        print(f'Loss percentage= {loss_percentage}')
+
+        # All hero stats
+        heroes_total_wins = df.where(match_win_condition_expression).groupby('hero_id')['hero_id'].count()
+        heroes_total_wins = pd.DataFrame({'hero_id':heroes_total_wins.index, 'wins':heroes_total_wins.values})
+        heroes_total_losses = df.where(match_loss_condition_expression).groupby('hero_id')['hero_id'].count()
+        heroes_total_losses = pd.DataFrame({'hero_id':heroes_total_losses.index, 'losses':heroes_total_losses.values})
+        heroes_matches_wins_losses = pd.merge(heroes_total_wins, heroes_total_losses, how="outer")
+        print(heroes_matches_wins_losses)
+
+    except FileNotFoundError:
+        print(f'could not find the .csv file at path: {pathToPlayerMatchesDataCsv}')
+
+
 def main():
-    logging.basicConfig(filename=f'main-log-{str(time.time())}.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)  
-    getMatchesDataCsv(108755293)
+    # Logging Config
+    # logging.basicConfig(filename=f'main-log-{str(time.time())}.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)  
+
+    # populatePlayerMatchesDataInCsv(108755293)
+    getOverallPlayerAnalysis("player-108755293-matches.csv")
+
 
 if __name__ == "__main__":
     main()
